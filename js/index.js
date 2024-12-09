d467f71d2839b5490f821b1d9cc395d603cf9c7a
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-08 20:13:09 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -10,6 +10,26 @@ function numberToText(number){
     return numberArr[number] || number
 }
 
+async function getData() {
+    const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    const coctail = await result.json()
+    console.log(coctail);
+    const randomDrink = coctail.drinks.sort(() => 0.5 - Math.random()).slice(0, 6)
+    randomDrink.forEach((element, index) => {
+        const box = document.createRange().createContextualFragment(`
+            
+            <div class="box-1">
+                <img src="${element.strDrinkThumb}" alt="">
+                <h4>project ${numberToText(index + 1)}</h4>
+            </div>
+            
+            `)
+            const porfolio_flex = document.querySelector('.porfolio-flex');
+            porfolio_flex.append(box)
+    });
+}
+getData()
+
 const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
     e.preventDefault()
     console.log("hola");
