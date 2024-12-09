dd5cb7b172a71ee41518cd26c52988a876a72419
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-09 18:43:09 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -33,4 +33,32 @@ getData()
 const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
     e.preventDefault()
     console.log("hola");
+    const inputArr = [
+        {id:"nombre", name:"Nombre"},
+        {id:"email", name:"Email"},
+        {id:"fecha", name:"Fecha"},
+        {id:"hora", name:"Hora"},
+        {id:"mensaje", name:"Mensaje"},
+    ]
+    for(let {id, name} of inputArr){
+        const elemento = document.getElementById(id);
+        if(elemento.value.trim() === "") {
+            return swal({
+                title: `El campo ${name} no puede estar vacío`,
+                icon: "error",
+                 })
+        }
+        if(id === "email" && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(elemento.value)){
+            return swal({
+                title: `El campo ${name} no tiene el formato correcto`,
+                icon: "error",
+                 })
+        }
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+    inputArr.forEach(({id})=> document.getElementById(id).value = "")
+    
 }
