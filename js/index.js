212e367ee12abc4dd0845626ae35cb15ce2b3599
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-09 20:06:32 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const numberArr = {
     5:"Cinco",
     6:"Seis"
 }
+
 function numberToText(number){
     return numberArr[number] || number
 }
@@ -29,6 +30,30 @@ async function getData() {
     });
 }
 getData()
+
+const techArr = ["Boostrap", "Css", "Html", "Sass", "Js", "Wordpress"]
+async function getData2() {
+    const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    const coctail = await result.json();
+    console.log(coctail);   
+        coctail.drinks.forEach((element, index)=> {
+            const nombre = techArr[index % techArr.length]; // Asigna un nombre del array
+            const box = document.createRange().createContextualFragment(`
+                
+                <div class="about-box">
+                    <img src="${element.strDrinkThumb}" alt="">
+                    <div class="about-skills-text">
+                        <h3>${nombre}</h3>
+                        <h4>80%</h4>
+                    </div>
+                </div>
+                
+                `)
+                const about = document.querySelector('.about-skills-flex');
+                about.append(box)
+        })
+}
+getData2()
 
 const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
     e.preventDefault()
